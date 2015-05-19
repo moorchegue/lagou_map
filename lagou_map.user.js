9ac5f3ee64bd567e37e92a1a22ab17578956d69f
murchik <mixturchik@gmail.com>
2015-05-20 02:51:15 +0800

    Trying to make it reusable.

--- lagou_map.user.js
+++ lagou_map.user.js
@@ -4,15 +4,20 @@
 // @description   Fuck endless lists, find a job on a map.
 // @homepage      http://github.com/moorchegue
 // @match         *://*.lagou.com/jobs/list*
+// @match         *://*.neitui.me/index.php?*
 // @version       0.0.2
 // @require       http://code.jquery.com/jquery-latest.min.js
+// @grant         GM_log
 // ==/UserScript==
 
-var bmap;
-var checkBMap;
-var checkMap = {};
+var HRWebsite = function() {
+    this.bmap = undefined;
+    this.checkBMap = undefined;
+    this.checkMap = {};
+    //this.$ = this.jQuery = jQuery.noConflict(true);
+}
 
-function getPageOpenings(page) {
+HRWebsite.prototype.getPageOpenings = function(page) {
     var links = [];
     var openings = page.getElementsByClassName('hot_pos_l');
     for (var i = 0; i < openings.length; i++) {
@@ -22,7 +27,7 @@ function getPageOpenings(page) {
     return links;
 }
 
-function getPagesCount() {
+HRWebsite.prototype.getPagesCount = function() {
     var pages = document.querySelectorAll('.Pagination a');
     if (pages.length == 0) {
         return 0;
@@ -31,50 +36,55 @@ function getPagesCount() {
     return parseInt(last_page.title);
 }
 
-function fetchOpening(url) {
+HRWebsite.prototype.fetchOpening = function(url) {
     //url = 'http://www.lagou.com/jobs/598412.html'
     console.log('fetching opening ' + url);
-    $.ajax({url: url}).done( function (output) {
-        var page = $(output);
-        addToMap(url, page);
+    this.$.ajax({url: url}).done( function (output) {
+        var page = this.$(output);
+        this.addToMap(url, page);
+        //this.appendLogo(url, page);
     });
 }
 
-function fetchPageOpenings(page) {
-    var openings = getPageOpenings(page);
+HRWebsite.prototype.fetchPageOpenings = function(page) {
+    var openings = this.getPageOpenings(page);
     for (var i = 0; i < openings.length; i++) {
-        fetchOpening(openings[i]);
+        this.fetchOpening(openings[i]);
     }
 }
 
-function fetchPage(url) {
-    $.ajax({ url: url, callback: fetchPageOpenings(page)});
+HRWebsite.prototype.fetchPage = function(url) {
+    this.$.ajax({ url: url, callback: this.fetchPageOpenings(page)});
 }
 
-function getCoordinates(page) {
+HRWebsite.prototype.getCoordinates = function(page) {
     return {
         lng: page.find('input[id=positionLng]').first().val(),
         lat: page.find('input[id=positionLat]').first().val()
     }
 }
 
-function getAddress(page) {
+HRWebsite.prototype.getAddress = function(page) {
     return page.find('dl[class=job_company] dd div').first().text()
 }
 
-function getCity(url) {
+HRWebsite.prototype.getCity = function(url) {
     return decodeURI(url).match(/(\?|\&)city=([^\&]+)/)[2];
 }
 
-function getCompanyName(page) {
+HRWebsite.prototype.getCompanyName = function(page) {
     return page.find('dl[class=job_company] h2.fl').first().contents().get(0).nodeValue;
 }
 
-function getOpeningId(url) {
+HRWebsite.prototype.getLogo = function(page) {
+    return page.find('dl[class=job_company] a img.b2').first()
+}
+
+HRWebsite.prototype.getOpeningId = function(url) {
     return url.match(/jobs\/([0-9]+)\.html/)[1];
 }
 
-function getMapScript(page) {
+HRWebsite.prototype.getMapScript = function(page) {
     var scripts = page.find('script');
     for (var i = 0; i < scripts.length; i++) {
         if (scripts[i].src.match('map\.baidu\.com')) {
@@ -83,39 +93,38 @@ function getMapScript(page) {
     }
 }
 
-function addToMap(url, page) {
+HRWebsite.prototype.addToMap = function(url, page) {
     console.log('page fetched!');
 
-    initializeMap(page);
+    this.initializeMap(page);
 
-    var coordinates = getCoordinates(page);
-    var address = getAddress(page);
-    var city = getCity(location);
-    var company = getCompanyName(page);
-    var id = getOpeningId(url);
+    var coordinates = this.getCoordinates(page);
+    var address = this.getAddress(page);
+    var city = this.getCity(location);
+    var company = this.getCompanyName(page);
+    var id = this.getOpeningId(url);
 
-    checkMap[id] = window.setInterval(function() {
+    this.checkMap[id] = window.setInterval(function() {
         console.log('check if map is ready');
-        if (typeof bmap != 'undefined') {
+        if (typeof this.bmap != 'undefined') {
             window.clearInterval(checkMap[id]);
-            console.log('map is ready for ' + id + ': ' + checkMap[id]);
+            console.log('map is ready for ' + id + ': ' + this.checkMap[id]);
             if (coordinates.lat && coordinates.lng) {
                 addByCoordinates(coordinates, company, address, city);
             } else {
-                addByAddress(company, address, city);
+                this.addByAddress(company, address, city);
             }
         }
     }, 1000);
-
 }
 
-function addPoint(point, company, address, city) {
+HRWebsite.prototype.addPoint = function(point, company, address, city) {
     //var icon = new BMap.Icon("markers.png", new BMap.Size(23, 25), {    
         //offset: new BMap.Size(10, 25),    
         //imageOffset: new BMap.Size(0, 0 - index * 25)
     //});
-    marker = new BMap.Marker(point);
-    bmap.addOverlay(marker);
+    var marker = new BMap.Marker(point);
+    this.bmap.addOverlay(marker);
 
     var tooltip = '<h4>' + company + '</h4>' + '<p>' + address + ' (' + city + ')</p>';
     var infoWindow = new BMap.InfoWindow(tooltip);
@@ -125,24 +134,24 @@ function addPoint(point, company, address, city) {
     });
 }
 
-function addByCoordinates(coordinates, company, address, city) {
+HRWebsite.prototype.addByCoordinates = function(coordinates, company, address, city) {
     console.log('add by coords' + coordinates.lng + ', ' + coordinates.lat)
     var point = new BMap.Point(coordinates.lng, coordinates.lat);
-    bmap.centerAndZoom(point, 11);
-    addPoint(point, company, address, city);
+    this.bmap.centerAndZoom(point, 11);
+    this.addPoint(point, company, address, city);
 }
 
-function addByAddress(company, address, city) {
+HRWebsite.prototype.addByAddress = function(company, address, city) {
     console.log('add by address' + address + ', ' + city);
     var gc = new BMap.Geocoder();
     gc.getPoint(address, function(point) {
         if (point) {
-            addByCoordinates(point, company, address, city);
+            this.addByCoordinates(point, company, address, city);
         }
     }, city);
 }
 
-function initializeMap(page) {
+HRWebsite.prototype.initializeMap = function(page) {
     // do this only once
     if (document.getElementById('map')) {
         console.log('map was already initialized');
@@ -154,27 +163,27 @@ function initializeMap(page) {
     var map_div = document.createElement('div');
     map_div.id = 'map';
     map_div.setAttribute('style', 'height: 500px; width: 100%;');
-    $('#workplaceSelect').after(map_div);
+    this.$('#workplaceSelect').after(map_div);
 
     // smartass proxying script
-    var proxy_script_url = getMapScript(page);
+    var proxy_script_url = this.getMapScript(page);
     console.log(proxy_script_url);
 
     // jsonp magic
-    $.ajax({url: proxy_script_url, dataType: 'jsonp'});
+    this.$.ajax({url: proxy_script_url, dataType: 'jsonp'});
 
     // XXX jsonpCallback only runs after result is loaded, not the code executed
-    checkBMap = window.setInterval(function() {
+    this.checkBMap = window.setInterval(function() {
         if (typeof BMap != 'undefined') {
-            window.clearInterval(checkBMap);
+            window.clearInterval(this.checkBMap);
             console.log('making a map!');
             // HACK delay is for webkit
             setTimeout(function() {
-                bmap = new BMap.Map('map');
-                bmap.addControl(new BMap.NavigationControl());
-                bmap.addControl(new BMap.MapTypeControl());
-                bmap.addControl(new BMap.OverviewMapControl());
-                bmap.enableScrollWheelZoom(true);
+                this.bmap = new BMap.Map('map');
+                this.bmap.addControl(new BMap.NavigationControl());
+                this.bmap.addControl(new BMap.MapTypeControl());
+                this.bmap.addControl(new BMap.OverviewMapControl());
+                this.bmap.enableScrollWheelZoom(true);
             }, 2000);
         }
     }, 1000);
@@ -182,16 +191,32 @@ function initializeMap(page) {
     console.log('map done');
 }
 
+
+var LaGou = function() {
+    HRWebsite.call(this);
+}
+LaGou.prototype = new HRWebsite();
+
+var NeiTui = function() {
+    HRWebsite.call(this);
+}
+NeiTui.prototype = new HRWebsite();
+
+
 function makeMap() {
     console.log('>>>>>>>>>>>>>>>>>>>>>');
 
-    this.$ = this.jQuery = jQuery.noConflict(true);
+    this.$ = jQuery = jQuery.noConflict(true);
 
-    //var openings = getPageOpenings(document);
-    //fetchOpening(openings[0]);
-    fetchPageOpenings(document);
+    console.log('000000000000000000000');
+    lagou = new Lagou();
+    console.log('111111111111111111111');
+    //var openings = lagou.getPageOpenings(document);
+    //lagou.fetchOpening(openings[0]);
+    lagou.fetchPageOpenings(document);
+    console.log('22222222222222222222');
 
-    var pages = getPagesCount();
+    var pages = lagou.getPagesCount();
     console.log(pages);
 
     console.log('----------------------');
@@ -201,7 +226,7 @@ function test() {
     console.log('Passed.');
 }
 
-if (this.window) {
+if (window) {
 	makeMap();
 } else {
 	test();
